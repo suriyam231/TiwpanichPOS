10af31f2d35d20718a53449de136a9cb181c41b3
Suriya AES <suriyam231@gmail.com>
2019-11-05 15:14:00 +0700

    Mooc Modal

--- Web/src/app/page/home/home.component.ts
+++ Web/src/app/page/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, TemplateRef, ElementRef, ViewChild } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { Key } from 'protractor';
+import { NzModalRef, NzModalService } from 'ng-zorro-antd';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,43 +9,129 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-Total :number =0
+
+
+  
+
+  constructor(private modalService: NzModalService) { }
+  Total: number = 0
   ngOnInit() {
 
     this.listOfData
-    for(let i = 0; i < this.listOfData.length;i++ ){
-      this.Total =   this.Total + this.listOfData[i].Price
+    for (let i = 0; i < this.listOfData.length; i++) {
+      this.Total = this.Total + this.listOfData[i].Price
     }
-    debugger
   }
 
   listOfData = [
     {
-      index : 1,
-      ProductName : 'เลย์',
-      number : 1,
-      Price : 20
-    },{
-      index : 2,
-      ProductName : 'ผงซักฟอง',
-      number : 2,
-      Price : 208
-    },{
-      index : 3,
-      ProductName : 'ปรับผ้านุ่ม',
-      number : 10,
-      Price : 180
-    },{
-      index : 4,
-      ProductName : 'ไม้กวาด',
-      number : 1,
-      Price : 40
-    },{
-      index : 5,
-      ProductName : 'ไม้ถูพื้น',
-      number : 1,
-      Price : 100
+      index: 1,
+      ProductName: 'เลย์',
+      number: 1,
+      Price: 20
+    }, {
+      index: 2,
+      ProductName: 'ผงซักฟอง',
+      number: 2,
+      Price: 208
+    }, {
+      index: 3,
+      ProductName: 'ปรับผ้านุ่ม',
+      number: 10,
+      Price: 180
+    }, {
+      index: 4,
+      ProductName: 'ไม้กวาด',
+      number: 1,
+      Price: 40
+    }, {
+      index: 5,
+      ProductName: 'ไม้ถูพื้น',
+      number: 1,
+      Price: 100
     }
   ];
+
+  Key
+  @ViewChild('Checkbill', { static: false }) checkbill: ElementRef;
+  @ViewChild('Getbill', { static: false }) getbill: ElementRef;
+  @ViewChild('Selectbill', { static: false }) selectbill: ElementRef;
+
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+
+      let el: HTMLElement = document.getElementById('Checkbill') as HTMLElement;
+      el.click();
+      event = undefined
+      this.Key = event
+    }
+    if (event.key === 'Escape') {
+      this.CheckModal = false
+    }
+    if(event.key === 'F2'){
+      let el: HTMLElement = document.getElementById('Getbill') as HTMLElement;
+      el.click();
+    }
+    if(event.key === 'F3'){
+      let el: HTMLElement = document.getElementById('Selectbill') as HTMLElement;
+      el.click();
+    }
+  }
+
+
+  CheckbillModal: NzModalRef;
+  tplModalButtonLoading = false;
+  htmlModalVisible = false;
+  disabled = false;
+  CheckModal = false;
+
+  Checkbill(tplTitle: TemplateRef<{}>, tplContent: TemplateRef<{}>, tplFooter: TemplateRef<{}>): void {
+    this.CheckbillModal = this.modalService.create({
+      nzTitle: tplTitle,
+      nzContent: tplContent,
+      nzFooter: tplFooter,
+      nzMaskClosable: false,
+      nzClosable: false,
+      nzOnOk: () => console.log('Click ok')
+
+    });
+    this.CheckModal = true
+  }
+
+  GetbillModal: NzModalRef;
+  GetbillModalButtonLoading = false;
+ 
+  Getbill(tplTitle: TemplateRef<{}>, tplContent: TemplateRef<{}>, tplFooter: TemplateRef<{}>): void {
+    this.CheckbillModal = this.modalService.create({
+      nzTitle: tplTitle,
+      nzContent: tplContent,
+      nzFooter: tplFooter,
+      nzMaskClosable: false,
+      nzClosable: false,
+      nzOnOk: () => console.log('Click ok')
+
+    });
+    this.CheckModal = true
+  }
+
+
+  SelectbillModal: NzModalRef;
+  SelectbillModalButtonLoading = false;
+ 
+  Selectbill(tplTitle: TemplateRef<{}>, tplContent: TemplateRef<{}>, tplFooter: TemplateRef<{}>): void {
+    this.CheckbillModal = this.modalService.create({
+      nzTitle: tplTitle,
+      nzContent: tplContent,
+      nzFooter: tplFooter,
+      nzMaskClosable: false,
+      nzClosable: false,
+      nzOnOk: () => console.log('Click ok')
+
+    });
+    this.CheckModal = true
+  }
+
+
 }
